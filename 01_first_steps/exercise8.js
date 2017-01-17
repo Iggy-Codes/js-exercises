fa970357909dea30f9d6af84e3537181378370b6
Iggy <amargos.ignasi@gmail.com>
2017-01-17 17:08:14 +0100

    exercise 8

--- 01_first_steps/exercise8.js
+++ 01_first_steps/exercise8.js
@@ -10,9 +10,11 @@
 
 function getRGB (colorHex) {
   var sRGB = 'rgb('
-  for (var i = 0; i < colorHex.length; i += 2) {
-
+  for (var i = 1; i < colorHex.length; i += 2) {
+    sRGB += parseInt(colorHex.substr(i, 2), 16) + ','
   }
+  sRGB = sRGB.substr(0, sRGB.length - 1) + ')'
+  return sRGB
 }
 
 // Hexadecimal enhanced
@@ -29,3 +31,27 @@ function getRGB (colorHex) {
 // >>> var a = getRGB ('#00FF00');
 // >>> a;
 // “rgb(0,255,0)   verde”;
+
+function getRGBimproved (colorHex) {
+  var sColor
+  switch (colorHex) {
+    case '#000000':
+      sColor = 'Black'
+      break
+    case '#FFFFFF':
+      sColor = 'White'
+      break
+    case '#FF0000':
+      sColor = 'Red'
+      break
+    case '#00FF00':
+      sColor = 'Green'
+      break
+    case '#0000FF':
+      sColor = 'Blue'
+      break
+    default:
+      sColor = 'Color name unknown'
+  }
+  return getRGB(colorHex) + '   ' + sColor
+}
